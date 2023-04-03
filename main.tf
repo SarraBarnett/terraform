e14298a4c82d0dbab78b187d2e96f43d1517e2fb
SarraBarnett <sarrabarnett@gmail.com>
2023-04-03 22:04:04 +0000

    added example code to autoformat

--- main.tf
+++ main.tf
@@ -275,3 +275,11 @@ resource "aws_security_group" "vpc-ping" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "random_string" "random" {
+  length      = 10
+  special     = true
+  min_numeric = 6
+  min_special = 2
+  min_upper   = 3
+}
