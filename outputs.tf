7c517bbb5a7025b12f834123cdf5dc3aaf818e62
SarraBarnett <sarrabarnett@gmail.com>
2023-04-13 07:20:01 +0000

    created file and added all outputs

---
+++ outputs.tf
@@ -0,0 +1,27 @@
+output "public_ip" {
+  value = module.server.public_ip
+}
+output "public_dns" {
+  value = module.server.public_dns
+}
+
+output "size" {
+  value = module.server.size
+}
+
+output "public_ip_server_subnet_1" {
+  value = module.server_subnet_1.public_ip
+}
+output "public_dns_server_subnet_1" {
+  value = module.server_subnet_1.public_dns
+}
+
+output "ec2_instance_arn" {
+  value     = aws_instance.web_server.arn
+  sensitive = true
+}
+
+output "phone_number" {
+  value     = var.phone_number
+  sensitive = true
+}
